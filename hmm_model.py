011e011b1201d59b41a8d178000d31b9d999b2c1
roise <roi.segel@mail.huji.ac.il>
2024-12-10 16:35:09 +0200

    find probabilities

--- hmm_model.py
+++ hmm_model.py
@@ -2,85 +2,64 @@ from pomegranate.hmm import DenseHMM
 from pomegranate.distributions import Categorical
 from collections import Counter
 import numpy as np
-
 import annotate_cpg
 
-#
-# # Sample training data: List of tuples (sequence, annotation)
-# training_data = [
-#     ("ACGTACGTACGT", "NNNNCCCCNNNN"),
-#     ("CGTACGTACGTA", "NNCCCCCNNNNN"),
-#     # Add more sequences and annotations here
-# ]
-#
-# # Step 1: Calculate emission probabilities for each state (CpG and non-CpG)
-# def calculate_emission_probabilities(training_data):
-#     nucleotides = ["A", "C", "G", "T"]
-#     cpg_counts = Counter()
-#     non_cpg_counts = Counter()
-#
-#     for sequence, annotation in training_data:
-#         for nucleotide, state in zip(sequence, annotation):
-#             if state == "C":  # CpG island
-#                 cpg_counts[nucleotide] += 1
-#             elif state == "N":  # Non-CpG island
-#                 non_cpg_counts[nucleotide] += 1
-#
-#     # Normalize counts to probabilities
-#     cpg_total = sum(cpg_counts.values())
-#     non_cpg_total = sum(non_cpg_counts.values())
-#
-#     cpg_probs = {nuc: cpg_counts[nuc] / cpg_total for nuc in nucleotides}
-#     non_cpg_probs = {nuc: non_cpg_counts[nuc] / non_cpg_total for nuc in nucleotides}
-#
-#     return cpg_probs, non_cpg_probs
-#
-# cpg_probs, non_cpg_probs = calculate_emission_probabilities(training_data)
-#
-# # Step 2: Define states using the emission probabilities
-# cpg_distribution = Categorical(cpg_probs)
-# non_cpg_distribution = Categorical(non_cpg_probs)
-#
-# # Step 3: Create and train the HMM
-# hmm = DenseHMM()
-# hmm.add_distributions([cpg_distribution, non_cpg_distribution])
-#
-# # Define transition probabilities (adjust based on your data)
-# hmm.add_edge(hmm.start, cpg_distribution, 0.5)
-# hmm.add_edge(hmm.start, non_cpg_distribution, 0.5)
-# hmm.add_edge(cpg_distribution, cpg_distribution, 0.9)
-# hmm.add_edge(cpg_distribution, non_cpg_distribution, 0.1)
-# hmm.add_edge(non_cpg_distribution, non_cpg_distribution, 0.9)
-# hmm.add_edge(non_cpg_distribution, cpg_distribution, 0.1)
-#
-# # Finalize the model
-# # hmm.bake()
-#
-# # Step 4: Train the HMM (optional, if you want to optimize transitions)
-# # Convert sequences to observation format
-# training_sequences = [list(sequence) for sequence, _ in training_data]
-# hmm.fit(training_sequences, algorithm="baum-welch")
-#
-# # Step 5: Test the HMM
-# test_sequence = list("ACGTACGTACGT")
-# log_probability, state_path = hmm.viterbi(test_sequence)
-#
-# print("Log Probability:", log_probability)
-# print("State Path:", " ".join(state.name for state in state_path))
+training_sequences_path = r"C:\Users\roise\OneDrive\Desktop\Hebrew U\CBIO\ex2\data\CpG-islands.2K.seq.fa.gz"
+training_labels_path = r"C:\Users\roise\OneDrive\Desktop\Hebrew U\CBIO\ex2\data\CpG-islands.2K.lbl.fa.gz"
+
+# Prepare training data and train model
+training_data = annotate_cpg.prepare_training_data(training_sequences_path, training_labels_path)
+train, test = training_data[:int(len(training_data) * 0.8)], training_data[int(len(training_data) * 0.8):]
 
 
-d1 = Categorical([[0.25, 0.25, 0.25, 0.25]])
-d2 = Categorical([[0.10, 0.40, 0.40, 0.10]])
+# find the emission probabilities for each state
+def get_emission_probs(data):
+    nucleotides = ["A", "C", "G", "T"]
+    cpg_counts = Counter()
+    non_cpg_counts = Counter()
+    cpg_probs = {}
+    non_cpg_probs = {}
+    for seq, annotation in data:
+        for nucleotide, state in zip(seq, annotation):
+            if state == "C":
+                cpg_counts[nucleotide] += 1
+            elif state == "N":
+                non_cpg_counts[nucleotide] += 1
+        cpg_total = sum(cpg_counts.values())
+        non_cpg_total = sum(non_cpg_counts.values())
+        cpg_probs = {nuc: cpg_counts[nuc] / cpg_total for nuc in nucleotides}
+        non_cpg_probs = {nuc: non_cpg_counts[nuc] / non_cpg_total for nuc in nucleotides}
+    return cpg_probs, non_cpg_probs
+
+def get_transition_probs(data):
+    transitions = {"CC": 0, "CN": 0, "NC": 0, "NN": 0}
+    for seq, annotation in data:
+        for i in range(1, len(annotation)):
+            transitions[annotation[i-1] + annotation[i]] += 1
+    total_from_C = transitions["CC"] + transitions["CN"]
+    total_from_N = transitions["NC"] + transitions["NN"]
+    return {"CC": transitions["CC"] / total_from_C, "CN": transitions["CN"] / total_from_C,
+            "NC": transitions["NC"] / total_from_N, "NN": transitions["NN"] / total_from_N}
+
+
+cpg, non_cpg = get_emission_probs(train)
+distribution_cpg = Categorical([[cpg["A"], cpg["G"], cpg["C"], cpg["T"]]])
+distribution_non_cpg = Categorical([[non_cpg["A"], non_cpg["G"], non_cpg["C"], non_cpg["T"]]])
+transition_probs = get_transition_probs(train)
+print(cpg)
+print(non_cpg)
+print(transition_probs)
 
 model = DenseHMM()
-model.add_distributions([d1, d2])
+model.add_distributions([distribution_cpg, distribution_non_cpg])
 
-model.add_edge(model.start, d1, 0.5)
-model.add_edge(model.start, d2, 0.5)
-model.add_edge(d1, d1, 0.9)
-model.add_edge(d1, d2, 0.1)
-model.add_edge(d2, d1, 0.1)
-model.add_edge(d2, d2, 0.9)
+model.add_edge(model.start, distribution_cpg, 0.5)
+model.add_edge(model.start, distribution_non_cpg, 0.5)
+model.add_edge(distribution_cpg, distribution_cpg, transition_probs["CC"])
+model.add_edge(distribution_cpg, distribution_non_cpg, transition_probs["CN"])
+model.add_edge(distribution_non_cpg, distribution_cpg, transition_probs["NC"])
+model.add_edge(distribution_non_cpg, distribution_non_cpg, transition_probs["NN"])
+
 
 sequence = 'CGACTACTGACTACTCGCCGACGCGACTGCCGTCTATACTGCGCATACGGC'
 X = np.array([[[['A', 'C', 'G', 'T'].index(char)] for char in sequence]])
@@ -91,29 +70,3 @@ y_hat = model.predict(X)
 print("sequence: {}".format(''.join(sequence)))
 print("hmm pred: {}".format(''.join([str(y.item()) for y in y_hat[0]])))
 
-
-training_sequences_path = r"C:\Users\roise\OneDrive\Desktop\Hebrew U\CBIO\ex2\data\CpG-islands.2K.seq.fa.gz"
-training_labels_path = r"C:\Users\roise\OneDrive\Desktop\Hebrew U\CBIO\ex2\data\CpG-islands.2K.lbl.fa.gz"
-
-# Prepare training data and train model
-training_data = annotate_cpg.prepare_training_data(training_sequences_path, training_labels_path)
-
-
-# find the emission probabilities for each state
-def get_probs(training_data):
-    nucleotides = ["A", "C", "G", "T"]
-    cpg_counts = Counter()
-    non_cpg_counts = Counter()
-    cpg_probs = {}
-    non_cpg_probs = {}
-    for sequence, annotation in training_data:
-        for nucleotide, state in zip(sequence, annotation):
-            if state == "C":
-                cpg_counts[nucleotide] += 1
-            elif state == "N":
-                non_cpg_counts[nucleotide] += 1
-        cpg_total = sum(cpg_counts.values())
-        non_cpg_total = sum(non_cpg_counts.values())
-        cpg_probs = {nuc: cpg_counts[nuc] / cpg_total for nuc in nucleotides}
-        non_cpg_probs = {nuc: non_cpg_counts[nuc] / non_cpg_total for nuc in nucleotides}
-    return cpg_probs, non_cpg_probs
