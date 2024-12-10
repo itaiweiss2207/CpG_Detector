116e46a98c71583f31a01ee1e617cb420155dbc3
roise <roi.segel@mail.huji.ac.il>
2024-12-10 17:18:17 +0200

    baseline markov model

--- hmm_model.py
+++ hmm_model.py
@@ -9,6 +9,8 @@ training_labels_path = r"C:\Users\roise\OneDrive\Desktop\Hebrew U\CBIO\ex2\data\
 
 # Prepare training data and train model
 training_data = annotate_cpg.prepare_training_data(training_sequences_path, training_labels_path)
+# remove any pair where the sequence contains a non-nucleotide character
+training_data = [pair for pair in training_data if all(char in "AGCT" for char in pair[0])]
 train, test = training_data[:int(len(training_data) * 0.8)], training_data[int(len(training_data) * 0.8):]
 
 
@@ -42,31 +44,74 @@ def get_transition_probs(data):
             "NC": transitions["NC"] / total_from_N, "NN": transitions["NN"] / total_from_N}
 
 
+def get_starting_probs(data):
+    cpg_starts = 0
+    non_cpg_starts = 0
+    for seq, annotation in data:
+        if annotation[0] == "C":
+            cpg_starts += 1
+        elif annotation[0] == "N":
+            non_cpg_starts += 1
+    total_starts = cpg_starts + non_cpg_starts
+    return cpg_starts / total_starts, non_cpg_starts / total_starts
+
+
+def loss_over_sequence(real_states, est_states):
+    loss = 0
+    for state, state_pred in zip(est_states, real_states):
+        if state != state_pred:
+            loss += 1
+    return loss / len(real_states)
+
+
+def loss_over_dataset(data, model):
+    loss = 0
+    for seq, annotation in data:
+        X = np.array([[[['A', 'C', 'G', 'T'].index(char)] for char in seq]])
+        pred_states = model.predict(X)
+        y_hat_states = ''.join(["C" if y.item() == 0 else "N" for y in pred_states[0]])
+        loss += loss_over_sequence(annotation, y_hat_states)
+    return loss / len(data)
+
+
 cpg, non_cpg = get_emission_probs(train)
 distribution_cpg = Categorical([[cpg["A"], cpg["G"], cpg["C"], cpg["T"]]])
 distribution_non_cpg = Categorical([[non_cpg["A"], non_cpg["G"], non_cpg["C"], non_cpg["T"]]])
 transition_probs = get_transition_probs(train)
+starting_probs = get_starting_probs(train)
 print(cpg)
 print(non_cpg)
 print(transition_probs)
+print(starting_probs)
 
 model = DenseHMM()
 model.add_distributions([distribution_cpg, distribution_non_cpg])
 
-model.add_edge(model.start, distribution_cpg, 0.5)
-model.add_edge(model.start, distribution_non_cpg, 0.5)
+model.add_edge(model.start, distribution_cpg, starting_probs[0] if starting_probs[0] > 0 else 0.000001)
+model.add_edge(model.start, distribution_non_cpg, starting_probs[1] if starting_probs[1] > 0 else 0.000001)
 model.add_edge(distribution_cpg, distribution_cpg, transition_probs["CC"])
 model.add_edge(distribution_cpg, distribution_non_cpg, transition_probs["CN"])
 model.add_edge(distribution_non_cpg, distribution_cpg, transition_probs["NC"])
 model.add_edge(distribution_non_cpg, distribution_non_cpg, transition_probs["NN"])
 
+for seq, annotation in test[0:2]:
+    X = np.array([[[['A', 'C', 'G', 'T'].index(char)] for char in seq]])
+    y_hat = model.predict(X)
+    y_hat_string = ''.join([str(y.item()) for y in y_hat[0]])
+    y_hat_states = ''.join(["C" if y.item() == 0 else "N" for y in y_hat[0]])
+    print("hmm orig: " + annotation)
+    print("hmm pred: " + y_hat_states)
+    print(loss_over_sequence(annotation, y_hat_states))
 
-sequence = 'CGACTACTGACTACTCGCCGACGCGACTGCCGTCTATACTGCGCATACGGC'
-X = np.array([[[['A', 'C', 'G', 'T'].index(char)] for char in sequence]])
-print(X.shape)
+print(loss_over_dataset(test, model))
 
-y_hat = model.predict(X)
+# sequence = 'CGACTACTGACTACTCGCCGACGCGACTGCCGTCTATACTGCGCATACGGC'
+# X = np.array([[[['A', 'C', 'G', 'T'].index(char)] for char in sequence]])
+# print(X.shape)
+#
+# y_hat = model.predict(X)
+#
+# print("sequence: {}".format(''.join(sequence)))
+# print("hmm pred: {}".format(''.join([str(y.item()) for y in y_hat[0]])))
 
-print("sequence: {}".format(''.join(sequence)))
-print("hmm pred: {}".format(''.join([str(y.item()) for y in y_hat[0]])))
 
